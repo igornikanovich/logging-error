e634bf579756e44587fceeb5ee8f693bc06d5607
IgorNikanovich <ignikanovich@gmail.com>
2019-11-21 22:11:49 +0300

    add pagination to error list

--- logging_errors/urls.py
+++ logging_errors/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import ApplicationListView, ApplicationDetailView
+from .views import ApplicationListView, ErrorApplicationListView
 
 urlpatterns = [
     path('apps/', ApplicationListView.as_view(), name='app_list'),
-    path('apps/<int:id>', ApplicationDetailView.as_view(), name='app-detail'),
+    path('apps/<int:id>', ErrorApplicationListView.as_view(), name='app-detail'),
 ]
--- logging_errors/views.py
+++ logging_errors/views.py
@@ -3,7 +3,8 @@ from uuid import uuid4
 import datetime as dt
 
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Application, Error
 
@@ -11,6 +12,7 @@ from .models import Application, Error
 class ApplicationListView(ListView):
     queryset = Application.objects.all()
     template_name = 'applications.html'
+    paginate_by = 5
 
     @staticmethod
     def post(request):
@@ -24,9 +26,10 @@ class ApplicationListView(ListView):
             return redirect('app_list')
 
 
-class ApplicationDetailView(DetailView):
-    model = Application
+class ErrorApplicationListView(ListView):
+    model = Error
     template_name = 'application_detail.html'
+    paginate_by = 5
 
     def get(self, request, *args, **kwargs):
         application = Application.objects.get(id=self.kwargs['id'])
@@ -36,41 +39,46 @@ class ApplicationDetailView(DetailView):
         type_error = request.GET.get('type')
         if type_error:
             error_list = Error.objects.filter(app_id=self.kwargs['id']).filter(type=type_error).order_by('-date')
+            error_list_charts = error_list
+            paginator = Paginator(error_list, self.paginate_by)
+            page = request.GET.get('page')
+            try:
+                error_list = paginator.page(page)
+            except PageNotAnInteger:
+                error_list = paginator.page(1)
+            except EmptyPage:
+                error_list = paginator.page(paginator.num_pages)
             first_error_date = Error.objects.filter(type=type_error).order_by('date').first()
             last_error_date = Error.objects.filter(type=type_error).order_by('date').last()
             first_date = first_error_date.date.date()
             last_date = last_error_date.date.date()
+            time = list()
+            count_errors = list()
             if first_date == last_date:
-                time = list()
-                count_errors = list()
-                for hour in range(25):
+                for hour in range(24):
                     time.append('%s' % hour)
                     count_hour_errors = 0
-                    for error in error_list:
+                    for error in error_list_charts:
                         if error.date.hour == hour:
                             count_hour_errors += 1
                     count_errors.append(count_hour_errors)
-                time.reverse()
-                count_errors.reverse()
             else:
                 delta_dates = last_date - first_date
                 total_days = delta_dates.days + 1
-                time = list()
-                count_errors = list()
                 for day_number in range(total_days):
                     current_date = (first_date + dt.timedelta(days=day_number))
                     time.append('%s' % current_date)
                     count_day_errors = 0
-                    for error in error_list:
+                    for error in error_list_charts:
                         if error.date.date() == current_date:
                             count_day_errors += 1
                     count_errors.append(count_day_errors)
-            time.reverse()
-            count_errors.reverse()
+                time.reverse()
+                count_errors.reverse()
         else:
             error_list, time, count_errors = None, None, None
-        return render(request, 'application_detail.html', {'errors_set': errors_set,
-                                                           'errors_list': error_list,
+        return render(request, 'application_detail.html', {'error_set': errors_set,
+                                                           'error_list': error_list,
                                                            'error_type': type_error,
                                                            'app_name': app_name,
                                                            'app_token': app_token,
