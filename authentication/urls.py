7b1520e35c9efef3fdcb40e41bfdcaa88726e363
IgorNikanovich <ignikanovich@gmail.com>
2019-11-22 13:44:53 +0300

    Update registration

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import RedirectView, registration
+from .views import RedirectView, UserRegistrationView
 
 urlpatterns = [
     path('', RedirectView.as_view(), name='index'),
-    path('register/', registration, name='register'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.views.generic import View, CreateView
 
 from .forms import UserForm
 
@@ -17,17 +17,21 @@ class RedirectView(View):
             return redirect('login')
 
 
-def registration(request):
-    registered = False
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
+class UserRegistrationView(CreateView):
+
+    def get(self, request, *args, **kwargs):
+        registered = False
+        return render(request, 'registration/registration.html', {'user_form': UserForm,
+                                                                  'registered': registered})
+
+    def post(self, request, *args, **kwargs):
+        user_form = UserForm(request.POST)
         if user_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
             user.save()
             registered = True
-    else:
-        user_form = UserForm()
-    return render(request, 'registration/registration.html',
-                  {'user_form': user_form,
-                   'registered': registered})
+            return render(request, 'registration/registration.html',
+                          {'user_form': user_form,
+                           'registered': registered})
+        return render(request, 'registration/registration.html', {'user_form': user_form})
